7450663f350a23b4d97581afe43070429317acb1
nabilabakhtawar <nabilabakhtawar@gmail.com>
2025-11-08 16:01:35 +0500

    React Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Hello from "./Hello"
+import myimage from "./assets/react.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, I'm a React App</h1>
+   <Hello/>
+   
+      <img src={myimage} width="50px"/>
     </div>
   );
 }
